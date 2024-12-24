0afb4c24da8dcd149f3da4c18dc796b7d149d20a
codepushed <mehrashubham216@gmail.com>
2024-12-25 00:30:05 +0530

    basic static data for bands, dials and size

---
+++ src/Static/index.js
@@ -0,0 +1,54 @@
+const watches = [
+    {
+        model: "series 10",
+        price: 429,
+        size: 46,
+        name: "Silver Aluminum",
+        bodyType: "Aluminum",
+        band: "Black Solo Loop",
+      },
+      {
+        model: "series 10",
+        price: 429,
+        size: 46,
+        name: "Rose Gold Aluminum",
+        bodyType: "Aluminum",
+        band: "Black Solo Loop",
+      },
+  {
+    model: "series 10",
+    price: 429,
+    size: 46,
+    name: "Jet Black Aluminum",
+    bodyType: "Aluminum",
+    band: "Black Solo Loop",
+  },
+  {
+    model: "series 10",
+    price: 749,
+    size: 46,
+    name: "Natural Titanium",
+    bodyType: "Titanium",
+    band: "Black Solo Loop",
+  },
+  {
+    model: "series 10",
+    price: 749,
+    size: 46,
+    name: "Gold Titanium",
+    bodyType: "Titanium",
+    band: "Black Solo Loop",
+  },
+  {
+    model: "series 10",
+    price: 749,
+    size: 46,
+    name: "Slate Titanium",
+    bodyType: "Titanium",
+    band: "Black Solo Loop",
+  },
+];
+
+// 46mm   42mm
+// aluminum titanium
+// bands
