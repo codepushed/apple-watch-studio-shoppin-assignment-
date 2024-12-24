b807e24e6b17130defe5c7b0c73cb30d75e682ed
codepushed <mehrashubham216@gmail.com>
2024-12-25 01:43:55 +0530

    watches faces and data

--- src/Static/index.js
+++ src/Static/index.js
@@ -1,20 +1,22 @@
-const watches = [
-    {
-        model: "series 10",
-        price: 429,
-        size: 46,
-        name: "Silver Aluminum",
-        bodyType: "Aluminum",
-        band: "Black Solo Loop",
-      },
-      {
-        model: "series 10",
-        price: 429,
-        size: 46,
-        name: "Rose Gold Aluminum",
-        bodyType: "Aluminum",
-        band: "Black Solo Loop",
-      },
+export const watches = [
+  {
+    model: "series 10",
+    price: 429,
+    size: 46,
+    name: "Silver Aluminum",
+    bodyType: "Aluminum",
+    band: "Black Solo Loop",
+    img: "/assets/dials/watch-case-46-aluminum-silver-nc-s10_VW_PF+watch-face-46-aluminum-silver-s10_VW_PF.png",
+  },
+  {
+    model: "series 10",
+    price: 429,
+    size: 46,
+    name: "Rose Gold Aluminum",
+    bodyType: "Aluminum",
+    band: "Black Solo Loop",
+    img: "/assets/dials/watch-case-46-aluminum-rosegold-nc-s10_VW_PF+watch-face-46-aluminum-rosegold-s10_VW_PF.png",
+  },
   {
     model: "series 10",
     price: 429,
@@ -22,6 +24,7 @@ const watches = [
     name: "Jet Black Aluminum",
     bodyType: "Aluminum",
     band: "Black Solo Loop",
+    img: "/assets/dials/watch-case-46-aluminum-jetblack-nc-s10_VW_PF+watch-face-46-aluminum-jetblack-s10_VW_PF.png",
   },
   {
     model: "series 10",
@@ -30,6 +33,7 @@ const watches = [
     name: "Natural Titanium",
     bodyType: "Titanium",
     band: "Black Solo Loop",
+    img: "/assets/dials/watch-case-46-titanium-natural-cell-s10_VW_PF+watch-face-46-titanium-natural-s10_VW_PF.png",
   },
   {
     model: "series 10",
@@ -38,6 +42,7 @@ const watches = [
     name: "Gold Titanium",
     bodyType: "Titanium",
     band: "Black Solo Loop",
+    img: "/assets/dials/watch-case-46-titanium-gold-cell-s10_VW_PF+watch-face-46-titanium-gold-s10_VW_PF.png",
   },
   {
     model: "series 10",
@@ -46,9 +51,335 @@ const watches = [
     name: "Slate Titanium",
     bodyType: "Titanium",
     band: "Black Solo Loop",
+    img: "/assets/dials/watch-case-46-titanium-slate-cell-s10_VW_PF+watch-face-46-titanium-slate-s10_VW_PF.png",
+  },
+];
+
+export const watchSize = ["46", "42"];
+
+export const watchBands = [
+  {
+    name: "Natural Milanese Loop",
+    type: "Stainless Steel",
+    isDialLimitedEdition: false,
+    img: "", //basic dials
+    price: 799,
+  },
+  {
+    name: "Gold Milanese Loop",
+    type: "Stainless Steel",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Slate Milanese Loop",
+    type: "Stainless Steel",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Natural Link Bracelet",
+    type: "Stainless Steel",
+    img: "",
+    price: 1049,
+  },
+  {
+    name: "Natural Link Bracelet",
+    type: "Stainless Steel",
+    img: "",
+    price: 1049,
+  },
+  {
+    name: "Gold Link Bracelet",
+    type: "Stainless Steel",
+    img: "",
+    price: 1049,
+  },
+  {
+    name: "Slate Link Bracelet",
+    type: "Stainless Steel",
+    img: "",
+    price: 1049,
+  },
+  {
+    name: "Ultramarine Sport Loop",
+    type: "Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Lake Green Sport Loop",
+    type: "Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Blue Cloud Sport Loop",
+    type: "Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Ink Sport Loop",
+    type: "Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Plum Sport Loop",
+    type: "Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Pride Edition Sport Loop",
+    type: "Sport Loop",
+    img: [
+      "watch-case-46-aluminum-silver-nc-s10_VW_PF+watch-face-46-pride-sport-loop-s10_VW_PF.png",
+      "watch-case-46-aluminum-rosegold-nc-s10_VW_PF+watch-face-46-pride-sport-loop-s10_VW_PF.png",
+      "watch-case-46-aluminum-jetblack-nc-s10_VW_PF+watch-face-46-pride-sport-loop-s10_VW_PF.png",
+      "watch-case-46-titanium-natural-cell-s10_VW_PF+watch-face-46-pride-sport-loop-s10_VW_PF.png",
+      "watch-case-46-titanium-gold-cell-s10_VW_PF+watch-face-46-pride-sport-loop-s10_VW_PF.png",
+      "watch-case-46-titanium-slate-cell-s10_VW_PF+watch-face-46-pride-sport-loop-s10_VW_PF.png",
+    ],
+    price: 749,
+  },
+  {
+    name: "Black Unity Sport Loop",
+    type: "Sport Loop",
+    img: [""], //this will contain unique dial editions
+    isDialLimitedEdition: true,
+    price: 749,
+  },
+  {
+    name: "Lake Green Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Stone Gray Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Starlight Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Plum Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Black Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Denim Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Light Blush Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Pride Edition Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Black Unity Sport Band - Unity Bloom",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Black Unity Sport Band",
+    type: "Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Black Magnetic Link",
+    type: "FineWoven",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Dark Taupe Magnetic Link",
+    type: "FineWoven",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Blackberry Magnetic Link",
+    type: "FineWoven",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Lake Green Braided Solo Loop",
+    type: "Braided Solo Loop",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Denim Braided Solo Loop",
+    type: "Braided Solo Loop",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Chartreuse Braided Solo Loop",
+    type: "Braided Solo Loop",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Magenta Braided Solo Loop",
+    type: "Braided Solo Loop",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Pride Edition Braided Solo Loop",
+    type: "Braided Solo Loop",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Black Unity Braided Solo Loop",
+    type: "Braided Solo Loop",
+    img: "",
+    price: 799,
+  },
+  {
+    name: "Star Fruit Solo Loop",
+    type: "Solo Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Ultramarine Solo Loop",
+    type: "Solo Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Lake Green Solo Loop",
+    type: "Solo Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Black Solo Loop",
+    type: "Solo Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Light Blush Solo Loop",
+    type: "Solo Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Black/Blue Nike Sport Loop",
+    type: "Nike Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Starlight/Pink Nike Sport Loop",
+    type: "Nike Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Grey/Blue Nike Sport Loop",
+    type: "Nike Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Green/Grey Nike Sport Loop",
+    type: "Nike Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Blue/Red Nike Sport Loop",
+    type: "Nike Sport Loop",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Volt Splash Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Magic Ember Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Magic Ember Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Midnight Sky Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Pure Platinum Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Desert Stone Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+  {
+    name: "Cargo Khaki Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
+  },
+
+  {
+    name: "Blue Flame Nike Sport Band",
+    type: "Nike Sport Band",
+    img: "",
+    price: 749,
   },
 ];
 
 // 46mm   42mm
 // aluminum titanium
 // bands
+
+// logic
+// if the dial is limited edition it will show the dial listing according to edition
+// other the same ordinary dials
