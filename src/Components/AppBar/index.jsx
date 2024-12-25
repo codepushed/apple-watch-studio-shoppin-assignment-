945bd523b11b30df5cf498bf4d547c2404ae03c1
codepushed <mehrashubham216@gmail.com>
2024-12-25 09:02:57 +0530

    header- collections and save btn

--- src/Components/AppBar/index.jsx
+++ src/Components/AppBar/index.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Modal from "../Modal";
 
 const AppBar = () => {
-    return(
-        <div className="appBarContainer">
-            <div className="appBarImgContainer">
-                <img src="/icons/apple-watch-design-studio-logo.jpeg" alt="logo" />
-            </div>
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Modal open={open} handleClose={handleClose} />
+      <div className="appBarContainer">
+        <div className="appBarImgContainer">
+          <img src="/icons/apple-watch-design-studio-logo.jpeg" alt="logo" />
         </div>
-    );
-}
+
+        <div onClick={() => setOpen(true)}>
+          <p className="appBarCollections">
+            Collections <KeyboardArrowDownIcon style={{ color: "#1d1d1f" }} />
+          </p>
+        </div>
+
+        <div className="watchStudioBtn appBarSaveBtn">
+          <button className="appleBlueBtn">Save</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default AppBar;
