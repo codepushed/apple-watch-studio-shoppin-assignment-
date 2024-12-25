9074c36c493a0930109bd391e18409e74506d94e
codepushed <mehrashubham216@gmail.com>
2024-12-25 12:01:46 +0530

    case button expand

--- src/Components/Tabs/index.jsx
+++ src/Components/Tabs/index.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Tabs = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  
   return (
     <div className="tabsContainer">
       <div className="tabs">
@@ -8,9 +10,12 @@ const Tabs = () => {
           <img src="/icons/watchSize.svg" alt="watch-size" />
           Size
         </buttons>
-        <buttons className="btnsWithIcons">
+        <buttons
+          className={`btnsWithIcons case-button ${isExpanded ? "expanded" : ""}`}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
           <img src="/icons/case.svg" alt="watch-case" />
-          Case
+          {isExpanded ? "Aluminum Titanium" : "Case"}
         </buttons>
         <buttons className="btnsWithIcons">
           <img src="/icons/bands.svg" alt="watch-bands" />
@@ -22,7 +27,3 @@ const Tabs = () => {
 };
 
 export default Tabs;
-
-{
-  /* <img src="/icons/watchSize.svg" /> */
-}
