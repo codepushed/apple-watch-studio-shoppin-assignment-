cf86107e3732b648e781de308bf1c40a7eff95c8
codepushed <mehrashubham216@gmail.com>
2024-12-25 16:37:06 +0530

    case type activness

--- src/Components/Tabs/index.jsx
+++ src/Components/Tabs/index.jsx
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 
 const Tabs = () => {
   const [isExpanded, setIsExpanded] = useState(false);
-  
+  const [isActive, setIsActive] = useState(false);
+
+  const handleActive = (material) => {
+    setIsActive(material);
+  };
+
   return (
     <div className="tabsContainer">
       <div className="tabs">
@@ -11,11 +16,30 @@ const Tabs = () => {
           Size
         </buttons>
         <buttons
-          className={`btnsWithIcons case-button ${isExpanded ? "expanded" : ""}`}
-          onClick={() => setIsExpanded(!isExpanded)}
+          className={`btnsWithIcons case-button ${
+            isExpanded ? "expanded" : ""
+          }`}
+          onClick={() => setIsExpanded(true)}
         >
           <img src="/icons/case.svg" alt="watch-case" />
-          {isExpanded ? "Aluminum Titanium" : "Case"}
+          {isExpanded ? (
+            <div className="caseTypes">
+              <p
+                onClick={() => handleActive("Aluminum")}
+                className={isActive === "Aluminum" ? "watchCaseTypeActive" : ""}
+              >
+                Aluminum
+              </p>
+              <p
+                onClick={() => handleActive("Titanium")}
+                className={isActive === "Titanium" ? "watchCaseTypeActive" : ""}
+              >
+                Titanium
+              </p>
+            </div>
+          ) : (
+            "Case"
+          )}
         </buttons>
         <buttons className="btnsWithIcons">
           <img src="/icons/bands.svg" alt="watch-bands" />
